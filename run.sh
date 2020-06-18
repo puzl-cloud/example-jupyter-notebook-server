291e9c62959c8588bbbb6dc560c3aff76a97395d
Sergey Monakhov <monakhov@puzl.ee>
2020-06-18 16:49:15 +0300

    dont install jupyter if using preset image in Puzl cloud

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ export PYTHONUSERBASE=/tmp
 
 #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /tmp/mykey.key -out /tmp/mycert.pem
 
-pip install --user -r requirements.txt
+#uncomment if not using preset image in Puzl cloud
+#pip install --user -r requirements.txt
 
 jupyter-notebook --notebook-dir="${__PUZL_WORKING_DIR}" --config=./jupyter_notebook_config.py
